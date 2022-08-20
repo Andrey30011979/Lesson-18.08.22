ef1e6767fd7655121834edbe06b5e7e02154a724
Andrey Marymov <marymovandrey078115@gmail.com>
2022-08-20 17:48:05 +0300

    task 002

---
+++ Task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int b  = int.Parse(Console.ReadLine());
+if (b*b == a)
+{
+    Console.WriteLine("Является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
