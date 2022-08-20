d797fcee8de8e814e2898e45a03d65e594402aeb
Andrey Marymov <marymovandrey078115@gmail.com>
2022-08-20 18:29:11 +0300

    task 004

---
+++ Task_004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input number");
+int number1 = int.Parse(Console.ReadLine());
+int i=0;
+int y = 2*(number1);
+
+Console.WriteLine("Ответ:");
+while (i<=y)
+{
+
+Console.WriteLine(-number1);
+    i++;
+    number1--;
+}
