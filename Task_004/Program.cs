b65317ed59830225a85ae32a1210c910ce5a6d09
Andrey Marymov <marymovandrey078115@gmail.com>
2022-08-20 18:42:15 +0300

    task 004 2

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -1,13 +1,24 @@
-﻿Console.WriteLine("Input number");
-int number1 = int.Parse(Console.ReadLine());
-int i=0;
-int y = 2*(number1);
+﻿//Console.WriteLine("Input number");
+//int number1 = int.Parse(Console.ReadLine());
+//int i=0;
+//int y = 2*(number1);
 
-Console.WriteLine("Ответ:");
-while (i<=y)
+//Console.WriteLine("Ответ:");
+//while (i<=y)
+//{
+
+//Console.WriteLine(-number1);
+    //i++;
+    //number1--;
+//}
+
+Console.WriteLine("Input number");
+string NumberStr;
+NumberStr = Console.ReadLine();
+int n = int.Parse(NumberStr);
+int a = n*(-1);
+while (a<=n)
 {
-
-Console.WriteLine(-number1);
-    i++;
-    number1--;
+    Console.WriteLine (a);
+    a = a + 1;
 }
