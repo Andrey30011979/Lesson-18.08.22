accb53eefc5fb60580fcc7b84ec78e3c518ff20a
Andrey Marymov <marymovandrey078115@gmail.com>
2022-08-26 00:17:06 +0300

    Task 4 Description

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -1,4 +1,12 @@
-﻿//Console.WriteLine("Input number");
+﻿// Напишите программу, которая на вход принимает
+// одно число (N),а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+// 4 → "-4,-3,-2,-1,0,1,2,3,4"
+
+// 2 → "-2,-1,0,1,2"
+
+//Console.WriteLine("Input number");
 //int number1 = int.Parse(Console.ReadLine());
 //int i=0;
 //int y = 2*(number1);
