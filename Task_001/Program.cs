9b53dfd05aaf08f9529122af1549b7357e702363
Andrey Marymov <marymovandrey078115@gmail.com>
2022-08-27 21:47:59 +0300

    Task 001 added an example

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("Please, input your number:");
+﻿/*Console.WriteLine("Please, input your number:");
 string numberStr;
 numberStr = Console.ReadLine();
 
 int number = int.Parse(numberStr);
 
 Console.WriteLine(number*number);
+*/
 //1. Напишите программу, которая на вход принимает два 
 //числа и проверяет, является ли первое число квадратом
 //второго.
@@ -12,3 +13,15 @@ Console.WriteLine(number*number);
 //a = 2, b = 10 → нет
 //a = 9, b = -3 → да
 //a = -3, b = 9 → нет
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+if (b*b == a)
+{
+    Console.WriteLine("Является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
